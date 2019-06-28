d25428b11aab4a2508ec4979807eea237661bfd9
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2019-06-28 14:23:08 -0500

    feat: initial commit

--- README.md
+++ README.md
@@ -1,30 +1,25 @@
-# node-project-skeleton
+# tar-bundler
 
-[![CircleCI](https://circleci.com/gh/jcoreio/node-project-skeleton.svg?style=svg)](https://circleci.com/gh/jcoreio/node-project-skeleton)
-[![Coverage Status](https://codecov.io/gh/jcoreio/node-project-skeleton/branch/master/graph/badge.svg)](https://codecov.io/gh/jcoreio/node-project-skeleton)
+[![CircleCI](https://circleci.com/gh/jcoreio/tar-bundler.svg?style=svg)](https://circleci.com/gh/jcoreio/tar-bundler)
+[![Coverage Status](https://codecov.io/gh/jcoreio/tar-bundler/branch/master/graph/badge.svg)](https://codecov.io/gh/jcoreio/tar-bundler)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
-Skeleton for libraries and apps targeting node.js >=8
+Generate tar.bz bundles using system bzip2 if possible, or self-contained bzip2 if needed
 
-## Quick start
+## Installation
 
 ```sh
-npx 0-60 clone https://github.com/jcoreio/node-project-skeleton.git
+yarn add @jcoreio/tar-bundler
 ```
 
-## Tools used
+## Usage
 
-- babel 7
-- mocha
-- chai
-- istanbul
-- nyc
-- eslint
-- flow
-- prettier
-- husky
-- semantic-release
-- renovate
-- Circle CI
-- Codecov.io
+```js
+const { bundle } = require('@jcoreio/tar-bundler')
+
+async function makeBundle() {
+  await bundle({ srcDir: 'myDir', destFile: 'archive.tar.bz2' })
+  console.log('successfully saved archive')
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,130 @@
 /* @flow */
 
-/* eslint-disable no-console, no-undef */
-console.log('Hello world!')
+import assert from 'assert'
+import fs from 'fs'
+
+import compressjs from 'compressjs'
+import { remove } from 'fs-extra'
+import logger from 'log4jcore'
+import { spawn } from 'promisify-child-process'
+import tar from 'tar-fs'
+
+const log = logger('tar-bundler')
+
+export async function bundle({
+  srcDir,
+  destFile,
+}: {
+  srcDir: string,
+  destFile: string,
+}): Promise<void> {
+  assert(srcDir, 'srcDir is required')
+  assert(destFile, 'destFile is required')
+  const tarFile =
+    (destFile.endsWith('.bz2') && destFile.substring(0, destFile.length - 4)) ||
+    `${destFile}.tar`
+  const tarStream = tar.pack(srcDir).pipe(fs.createWriteStream(tarFile))
+  await waitForStream(tarStream, 'close')
+
+  try {
+    log.info('compressing with system bzip2...')
+    await remove(destFile)
+    await spawn('bzip2', [tarFile], { stdio: 'inherit' })
+  } catch (err) {
+    log.info('system bzip2 did not work, trying built-in bzip2...')
+    const readFd = fs.openSync(tarFile, 'r')
+    const writeFd = fs.openSync(destFile, 'w')
+    compressjs.Bzip2.compressFile(makeInStream(readFd), makeOutStream(writeFd))
+    fs.closeSync(readFd)
+    fs.closeSync(writeFd)
+  }
+}
+
+async function waitForStream(
+  stream: Object,
+  doneEvent: string = 'close'
+): Promise<void> {
+  await new Promise((resolve: Function, reject: Function) => {
+    stream.on(doneEvent || 'finish', resolve)
+    stream.on('error', (err: any) => reject(err))
+  })
+}
+
+function makeInStream(in_fd: number): Object {
+  var stream = new compressjs.Stream()
+  var stat = fs.fstatSync(in_fd)
+  if (stat.size) {
+    stream.size = stat.size
+  }
+  stream.buffer = new Buffer(4096)
+  stream.filePos = null
+  stream.pos = 0
+  stream.end = 0
+  stream._fillBuffer = function() {
+    this.end = fs.readSync(
+      in_fd,
+      this.buffer,
+      0,
+      this.buffer.length,
+      this.filePos
+    )
+    this.pos = 0
+    if (this.filePos !== null && this.end > 0) {
+      this.filePos += this.end
+    }
+  }
+  stream.readByte = function(): number {
+    if (this.pos >= this.end) {
+      this._fillBuffer()
+    }
+    if (this.pos < this.end) {
+      return this.buffer[this.pos++]
+    }
+    return -1
+  }
+  stream.read = function(
+    buffer: Buffer,
+    bufOffset: number,
+    length: number
+  ): number {
+    if (this.pos >= this.end) {
+      this._fillBuffer()
+    }
+    var bytesRead = 0
+    while (bytesRead < length && this.pos < this.end) {
+      buffer[bufOffset++] = this.buffer[this.pos++]
+      bytesRead++
+    }
+    return bytesRead
+  }
+  stream.seek = function(seek_pos: number) {
+    this.filePos = seek_pos
+    this.pos = this.end = 0
+  }
+  stream.eof = function(): boolean {
+    if (this.pos >= this.end) {
+      this._fillBuffer()
+    }
+    return this.pos >= this.end
+  }
+  stream.buffer.fill(0)
+  return stream
+}
+
+function makeOutStream(out_fd: number): Object {
+  var stream = new compressjs.Stream()
+  stream.buffer = new Buffer(4096)
+  stream.pos = 0
+  stream.flush = function() {
+    fs.writeSync(out_fd, this.buffer, 0, this.pos)
+    this.pos = 0
+  }
+  stream.writeByte = function(_byte: number) {
+    if (this.pos >= this.buffer.length) {
+      this.flush()
+    }
+    this.buffer[this.pos++] = _byte
+  }
+  stream.buffer.fill(0)
+  return stream
+}
